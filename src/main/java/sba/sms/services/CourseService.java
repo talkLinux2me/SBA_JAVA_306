19d342794b632cb9a098e306337854ab262b6bea
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-08-12 14:32:36 -0400

    try and catch and finishing Class

--- src/main/java/sba/sms/services/CourseService.java
+++ src/main/java/sba/sms/services/CourseService.java
@@ -66,6 +66,58 @@ public class CourseService implements CourseI {
 
     // getCourseById(2) -> Course -> courseId = 2, courseName = English
 
+    public Course getCourseById(int courseId) {
+
+        Course course = null;
+        Transaction transaction = null;
+
+        try {
+            // We're starting transaction here
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+
+            // We're getting a specific course with our courseId from our database
+            course = session.get(Course.class, courseId);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println(e);
+        }
+
+        return course;
+    }
+
+    public List<Course> getAllCourses() {
+        // TODO: MIGHT NOT NEED THIS?
+        Transaction transaction = null;
+        try {
+            // We're starting the transaction here!
+            session = factory.openSession();
+            // TODO: MIGHT NOT NEED THIS?
+            transaction = session.beginTransaction();
+
+            // Making a SQL QUERY
+            String hql = "SELECT course FROM Course course";
+
+            // Sending query to our database
+            TypedQuery<Course> query = session.createQuery(hql, Course.class);
+
+            // returning whatever we got from our database using our query
+            return query.getResultList();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+            return new ArrayList<>();
+        }
+
+    }
+
 }
+
 
 
