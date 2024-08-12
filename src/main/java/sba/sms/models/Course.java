6c958dc9b72d4bf0194b1ecb9ad66be6acb54a87
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-08-12 11:54:27 -0400

    imports

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -7,7 +7,16 @@ import lombok.experimental.FieldDefaults;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
-
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.Helper;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.Helper;
+import java.util.HashSet;
+import java.util.Set;
 /**
  * Course is a POJO, configured as a persistent class that represents (or maps to) a table
  * name 'course' in the database. A Course object contains fields that represent course
