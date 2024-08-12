bccaf35406b4d07514fdf8d83f0277249dd7b99c
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-08-12 11:52:48 -0400

    first commmit

---
+++ src/main/java/sba/sms/models/Student.java
@@ -0,0 +1,26 @@
+package sba.sms.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+/**
+ * Student is a POJO, configured as a persistent class that represents (or maps to) a table
+ * name 'student' in the database. A Student object contains fields that represent student
+ * login credentials and a join table containing a registered student's email and course(s)
+ * data. The Student class can be viewed as the owner of the bi-directional relationship.
+ * Implement Lombok annotations to eliminate boilerplate code.
+ */
+
+public class Student {
+
+
+    }
+
+
+
